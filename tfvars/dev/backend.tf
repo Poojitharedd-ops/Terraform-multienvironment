0049f9fb94ca4653b0e6c6a3d037d46d10c35b62
poojitha <poojithadspr@gmail.com>
2025-02-07 15:29:15 +0530

    provider

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "poojitha-remote-state-dev"
+key = "tfvars-demo-dev"
+region = "us-east-1"
+dynamodb_table = "poojitha-locking-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "poojitha-remote-state-prod"
+key = "tfvars-demo-prod"
+region = " us-east-1"
+dynamodb_table = "poojitha-locking-prod"
